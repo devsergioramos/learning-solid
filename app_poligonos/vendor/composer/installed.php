4863f61a647ca42c477ecb6436114fb522998393
Sergio Ramos <sergio.ramos@pgmais.com.br>
2021-05-14 14:03:58 -0400

    init projeto poligonos

---
+++ app_poligonos/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e98774055d5fd897fc877e5a0e996f157766d5f1',
+    'name' => 'sergio/app_poligonos',
+  ),
+  'versions' => 
+  array (
+    'sergio/app_poligonos' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e98774055d5fd897fc877e5a0e996f157766d5f1',
+    ),
+  ),
+);
